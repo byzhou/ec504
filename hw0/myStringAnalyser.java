b70668dac1699e0cc9cf3675e5c81e8c2a029832
bob-icsg <bobzhou@bu.edu>
2015-09-08 21:55:48 -0400

    AUTOCOMMIT bobzhou@bobzhou-ThinkCentre-M73:Tue Sep  8 09:15:29 EDT 2015

--- hw0/myStringAnalyser.java
+++ hw0/myStringAnalyser.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class myStringAnalyser { 
     public static void main (String[] args) {
-        System.out.println("hello World");
+        // System.out.println("hello World");
         stringAnalyse(args[0],Integer.parseInt(args[1]));
     }
 
@@ -19,7 +19,7 @@ public class myStringAnalyser {
 
         // read each character in the string
         for (char ch: str.toCharArray() ){
-            System.out.println(ch + "\n");
+            //System.out.println(ch + "\n");
             if (chaList.contains(ch)){
                 modifiedValue = intList.get(chaList.indexOf(ch)) + 1;
                 intList.set(chaList.indexOf(ch),modifiedValue);
@@ -29,61 +29,24 @@ public class myStringAnalyser {
                 intList.add(1);
             }
         }
-       
-        // initialization of result array 
-         for (int i = 0 ; i < size ; i ++ ) {
-             resultInt[i]              = intList.get(i) ;
-             resultCha[i]              = chaList.get(i) ;
-         }
-         for (int j = 0 ; j < size ; j ++ ) {
-             if (resultInt[j] < resultInt[minIndex]){
-                 minIndex       = j ;
-                 minCount       = resultInt[j] ;
-             }
-         }
-         // putting the result in the result array
-         for (int i = 0 ; i < chaList.size() ; i ++ ) {
-             boolean exist      = false ;
-             for ( int k = 0; k < chaList.size() ; k++ ) {
-                if (chaList.get(i) == resultCha[k]) {
-                    exist               = true ;
+        
+        for ( int i = 0 ; i < size ; i ++ ) {
+            if ( size < chaList.size() ) {
+                int largestValueIndex       = 0 ;
+                for ( int j = 0 ; j < intList.size() ; j ++ ) {
+                    if ( intList.get(j) > intList.get(largestValueIndex) ) {
+                        largestValueIndex = j ;
+                    }
                 }
-             }
-             if ( intList.get(i) > minCount && exist == false ) {
-                 resultInt[minIndex]    = intList.get(i); 
-                 resultCha[minIndex]    = chaList.get(i); 
-                 // update the result array
-                 for (int j = 0 ; j < size ; j ++ ) {
-                     if (resultInt[j] < resultInt[minIndex]){
-                         minIndex       = j ;
-                         minCount       = resultInt[j] ;
-                     }
-                 }
-             }
-         }
-         // sorting
-         for ( int i = 0 ; i < (size - 1) ; i ++ ) {
-             int tmpInt = 0 ;
-             char tmpCha = 'c' ;
-             for ( int j = 1 ; j < size ; j ++ ) {
-                 if ( resultInt[j] > resultInt[i] ) {
-                     // exchange
-                     tmpInt         = resultInt[j];
-                     resultInt[i]   = resultInt[j];
-                     resultInt[j]   = tmpInt;
-                     tmpCha         = resultCha[j];
-                     resultCha[i]   = resultCha[j];
-                     resultCha[j]   = tmpCha;
-                 }
-             }
-         }
+                System.out.println ( chaList.get(largestValueIndex) ) ;    
+                chaList.remove(largestValueIndex);
+                intList.remove(largestValueIndex);
 
-        System.out.println(Arrays.toString(chaList.toArray()));
-        System.out.println(Arrays.toString(intList.toArray()));
-        System.out.println("This is results");
-        for (int i = 0 ; i < size ; i ++ ) {
-            System.out.println(resultCha[i]);
+            }
         }
+
+        // System.out.println(Arrays.toString(chaList.toArray()));
+        // System.out.println(Arrays.toString(intList.toArray()));
         
     }
 }
