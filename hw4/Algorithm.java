614d648df36b2e8d584356a9cd6a3cdceb034549
bob-icsg <bobzhou@bu.edu>
2015-11-27 15:06:31 -0500

    hw4

---
+++ hw4/Algorithm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shortestpath;
+
+import java.util.List;
+
+public class Algorithm
+{
+
+    Node source;
+    List<Node> nodeList;
+
+    Algorithm(Node source, List<Node> v)
+    {
+        this.source = source;
+        this.nodeList = v;
+    }
+
+    void modifiedDijkstra()
+    {
+        // This is where you write your version of Dijkstra's modified algorithm
+    }
+}
