f49158d1c124f72f6bb9dddbf314c4ce54392495
bob-icsg <bobzhou@bu.edu>
2015-11-29 15:30:11 -0500

    hand in version

--- hw4/Edge.java
+++ hw4/Edge.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package shortestpath;
+package shortestpath;
 
 public class Edge {
 	Node 	start;	/* initial vertex of this edge */
